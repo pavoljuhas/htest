7fd31d25208a3e225468704dfc0d6d25ced24825
Pavol Juhas <pavol.juhas@gmail.com>
2015-12-09 14:46:20 -0500

    Move files for integration with scikit-xray.

--- setup.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-        ext_modules = cythonize("htest.pyx"),
-        include_dirs = [numpy.get_include()],
-        #ext_modules = cythonize("convolve.pyx")
-)
